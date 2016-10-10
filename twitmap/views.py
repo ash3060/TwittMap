7edeff99a66145800b48204d749e58f4e7b8e35b
s0260 <593359434@qq.com>
2016-10-10 16:14:04 -0400

    1st commit

---
+++ twitmap/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+import datetime
+
+
+import urllib
+import urllib2
+import re
+
+def a(request):
+    return render_to_response('a.html')
