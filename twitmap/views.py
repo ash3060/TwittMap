9823e13464836877de3334228e55f95fb6019490
s0260 <593359434@qq.com>
2016-10-19 15:55:58 -0400

    infowindow added

--- twitmap/views.py
+++ twitmap/views.py
@@ -14,10 +14,11 @@ import json
 
 
 class poi(object):
-    def __init__(self, _lat, _lng, _tags):
+    def __init__(self, _lat, _lng, _usr, _txt):
         self.lat = _lat
         self.lng = _lng
-        self.tags = _tags
+        self.usr = _usr
+        self.txt = _txt
 
     def toJSON(self):
         return json.dumps(self.__dict__)
@@ -33,8 +34,9 @@ def loaddata():
         lat = hit['coordinates'][1]
         lng = hit['coordinates'][0]
         text = hit['text']
+        usr = hit['username']
         # print lat,', ', lng
-        data.append(poi(lat, lng, text.lower().split(' ')))
+        data.append(poi(lat, lng, usr, text))
     return data
 
 
@@ -47,14 +49,15 @@ def search(request):
         keyword = request.GET['keyword']
     else:
         return render_to_response('')
+    keyword = keyword.lower()
     List = loaddata()
     poi_list = []
     for item in List:
-        if keyword in item.tags:
+        if keyword in item.txt.lower().split(' '):
             poi_list = poi_list + [item.toJSON()]
 
     result = json.dumps(poi_list)
-    print result
+    # print result
     # return HttpResponse({'poi_list': poi_list}, content_type='application/json')
     # return render_to_response("result.html", {'poi_list': poi_list})
     return HttpResponse(result, content_type='application/json')
